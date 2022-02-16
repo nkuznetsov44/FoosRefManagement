a0741b6ab5f019d1074ec75580f3d6bee4816d0e
Nikita Kuznetsov <nkuznetsov44@gmail.com>
2022-02-16 18:52:38 +0300

    display referees prototype

--- foosrefmanagement-ui/src/pages/referees/Referees.js
+++ foosrefmanagement-ui/src/pages/referees/Referees.js
@@ -1,6 +1,26 @@
+import React from 'react';
+import axios from 'axios';
+import { DataGrid } from 'devextreme-react';
+
 const Referees = () => {
+    const [referees, setReferees] = React.useState([]);
+
+    React.useEffect(() => {
+        (async () => {
+            const { data } = await axios.get('/api/referees');
+            setReferees(data);
+        })();
+    }, [])
+
     return (
-        <div><h1>Referees</h1></div>
+        <React.Fragment>
+            <h1>Referees</h1>
+            <div className="refereesTableWrapper">
+                <DataGrid
+                    dataSource={referees}>
+                </DataGrid>
+            </div>
+        </React.Fragment>
     );
 }
 
--- foosrefmanagement/app/views.py
+++ foosrefmanagement/app/views.py
@@ -1,5 +1,23 @@
 from django.http import HttpResponse
+from .models import Referee
+import json
 
 
 def index(request):
     return HttpResponse('foosrefmanagement/app index')
+
+
+def referees(request):
+    referees = Referee.objects.all()
+
+    def serialize_referee(referee):
+        return {
+            'first_name': referee.first_name,
+            'last_name': referee.last_name,
+            'email': referee.email,
+            'languages': referee.languages,
+            'rank': referee.rank
+        }
+    
+    referees_json = json.dumps(list(map(serialize_referee, referees)))
+    return HttpResponse(referees_json)
