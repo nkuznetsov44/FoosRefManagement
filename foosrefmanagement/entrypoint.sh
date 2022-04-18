4acb92cb794c3ca5db1caaebd070e485e8a166e7
Nikita Kuznetsov <nkuznetsov44@gmail.com>
2022-04-18 23:33:18 +0300

    add node build, nginx serve static

--- foosrefmanagement/entrypoint.sh
+++ foosrefmanagement/entrypoint.sh
@@ -10,7 +10,7 @@ echo "PostgreSQL started"
 
 # python manage.py flush --no-input
 python manage.py migrate --noinput
-python manage.py collectstatic --no-input --clear
+# python manage.py collectstatic --no-input --clear
 python manage.py createsuperuser --noinput
 
 exec "$@"
